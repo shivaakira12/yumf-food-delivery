6b42748762c2468602619b85e4a489c3e966b5b5
ShivaShanker7712 <shivashanker7712@gmail.com>
2024-03-15 11:00:20 +0530

    Creating Home page and header page

--- froentend/src/App.jsx
+++ froentend/src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/Navbar/NavBar";
-
+import Cart from "./pages/Cart/Cart";
+import { Home } from "./pages/Home/Home";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="app">
       <NavBar />
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/cart" element={<Cart></Cart>}></Route>
+        <Route path="/order" element={<PlaceOrder></PlaceOrder>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ froentend/src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "../../components/Navbar/Header/Header";
+import "./Home.css";
+export const Home = () => {
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  );
+};
