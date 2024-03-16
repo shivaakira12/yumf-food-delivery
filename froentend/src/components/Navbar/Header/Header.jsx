c2e2651becabaaf1210a4f2860d554fb0901c41e
ShivaShanker7712 <shivashanker7712@gmail.com>
2024-03-16 10:40:31 +0530

    .

--- froentend/src/components/Navbar/Header/Header.jsx
+++ froentend/src/components/Navbar/Header/Header.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 import "./Header.css";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order Your favorite Food Here</h2>
+        <p>
+          Dive into our curated selection of recipes, explore cuisines from
+          around the globe, and find inspiration for your next culinary
+          adventure. We're your one-stop shop for everything food, from easy
+          weeknight meals to show-stopping feasts.
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- froentend/src/pages/Home/Home.jsx
+++ froentend/src/pages/Home/Home.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
 import Header from "../../components/Navbar/Header/Header";
 import "./Home.css";
 export const Home = () => {
+  const [category, setCategory] = useState("All");
   return (
     <div>
       <Header></Header>
+      <ExploreMenu category={category} setCategory={setCategory}></ExploreMenu>
     </div>
   );
 };
