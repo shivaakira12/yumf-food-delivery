375b214218b1a0bceba8363ccaf2df557abdd835
Shiva <63032037+shivaakira12@users.noreply.github.com>
2024-06-29 15:09:18 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -43,7 +43,7 @@ const StoreContextProvider = (props) => {
   };
 
   const fetchFoodList = async () => {
-    const response = await axios.get("https://yumf-food-delivery-backend.onrender.com");
+    const response = await axios.get("url+/api/food/list");
     setFoodList(response.data.data);
   };
 
